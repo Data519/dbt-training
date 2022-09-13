52797fdc1e08dad34123afa46f858c2a21e97e64
Richard Nguyen <nguyenk02@outlook.com>
2022-09-13 15:50:04 +0000

    develop reporting profits by customer, profits by products

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -3,9 +3,9 @@ SELECT o.order_id, o.order_date, o.shipdate, o.shipmode,
        o.ordercostprice, o.ordersellingprice,
        o.ordersellingprice - o.ordercostprice orderprofit,       
        --from raw customer
-       c.customername, c.segment, c.country,
+       c.customerid, c.customername, c.segment, c.country,
        --from Product
-       p.category, p.productname, p.subcategory
+       p.productid, p.category, p.productname, p.subcategory
   FROM {{ref('raw_orders')}} as o
 LEFT JOIN {{ref('raw_customer')}} as c
 on o.customerid = c.customerid
