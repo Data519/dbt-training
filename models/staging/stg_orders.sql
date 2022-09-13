fdf2ebbbb21a6f0eac3716e613248bc0cefc92ea
Richard Nguyen <nguyenk02@outlook.com>
2022-09-13 15:36:01 +0000

    develop staging

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,13 @@
+
+SELECT o.order_id, o.order_date, o.shipdate, o.shipmode, 
+       o.ordercostprice, o.ordersellingprice,
+       o.ordersellingprice - o.ordercostprice orderprofit,       
+       --from raw customer
+       c.customername, c.segment, c.country,
+       --from Product
+       p.category, p.productname, p.subcategory
+  FROM {{ref('raw_orders')}} as o
+LEFT JOIN {{ref('raw_customer')}} as c
+on o.customerid = c.customerid
+LEFT JOIN {{ref('raw_product')}} as p
+on o.productid = p.productid
